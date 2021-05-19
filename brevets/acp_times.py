359ea075a2531413516efe8786ef4cef1024e0c9
derekpinto2k <dpinto@uoregon.edu>
2021-05-18 22:04:29 -0700

    no message

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -1,10 +1,12 @@
 """
 Open and close time calculations
 for ACP-sanctioned brevets
-following rules described at https://rusa.org/octime_alg.html
-and https://rusa.org/pages/rulesForRiders
+following rules described at
+https://rusa.org/octime_alg.html
+https://rusa.org/pages/rulesForRiders
 """
 import arrow
+
 
 
 #  You MUST provide the following two functions
@@ -26,7 +28,26 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    brevet_open_time = 0
+
+    brevet_dist_times = {'200': 353, '400': 728, '600': 1128, '1000': 1985}
+
+    if control_dist_km < 200:
+        brevet_open_time = (control_dist_km/34*60)
+
+    if 200 <= control_dist_km < 400:
+        brevet_open_time = ((control_dist_km%200)/32*60)+brevet_dist_times.get('200')
+
+    if 400 <= control_dist_km < 600:
+        brevet_open_time = ((control_dist_km%400)/30*60)+brevet_dist_times.get('400')
+
+    if 600 <= control_dist_km < 1000:
+        brevet_open_time = ((control_dist_km%600)/28*60)+brevet_dist_times.get('600')
+
+    if control_dist_km == 1000:
+        brevet_open_time = brevet_dist_times.get('1000')
+
+    return brevet_start_time.shift(minutes=brevet_open_time)
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -41,4 +62,25 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        A date object indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now()
+    brevet_close_time = 60
+
+    brevet_dist_times = {'200': 800, '400': 1600, '600': 2400, '1000': 4500}
+
+    if 0 < control_dist_km < 200:
+        brevet_close_time = control_dist_km/15*60
+
+    if 200 <= control_dist_km < 400:
+        brevet_close_time = ((control_dist_km%200)/15*60)+brevet_dist_times.get('200')
+
+    if 400 <= control_dist_km < 600:
+        brevet_close_time = ((control_dist_km%400)/15*60)+brevet_dist_times.get('400')
+
+    if 600 <= control_dist_km < 1000:
+        brevet_close_time = ((control_dist_km%600)/11.428*60)+brevet_dist_times.get('600')
+
+    if control_dist_km == 1000:
+        brevet_close_time = brevet_dist_times.get('1000')
+
+    return brevet_start_time.shift(minutes=brevet_close_time)
+
+
